0eafa1ac8f98899879bd97183202b6ad23c117a0
perlancar (@pc-office) <perlancar@gmail.com>
2015-04-28 21:00:01 +0700

    Initial commit

---
+++ lib/App/IndonesianBankingUtils.pm
@@ -0,0 +1,24 @@
+package App::IndonesianBankingUtils;
+
+use 5.010001;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLIs related to Indonesian banking
+
+=head1
+
+This distribution contains several CLI's related to Indonesian banking:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Finance::Bank::ID::BCA>
+
+L<Finance::Bank::ID::BPRKS>
+
+L<Finance::Bank::ID::Mandiri>
